8039df1c170eee44e68d3a08f3f3852232915be7
mok-choi <hyunmok.choi@gmail.com>
2024-01-14 21:28:58 +0900

    Init

---
+++ src/main/java/com/example/olebackend/OleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.olebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/olebackend/OleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
