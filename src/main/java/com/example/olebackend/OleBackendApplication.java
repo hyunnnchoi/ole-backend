44a4cc11078955c975c892ca3b1ab090a7f80970
DESKTOP-DGFPD6H\SHIN HEEMIN <sinhimin11@naver.com>
2024-01-17 13:10:50 +0900

    chore: 패키지 구조 설계

--- src/main/java/com/example/olebackend/OleBackendApplication.java
+++ src/main/java/com/example/olebackend/OleBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OleBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OleBackendApplication.class, args);
 	}
