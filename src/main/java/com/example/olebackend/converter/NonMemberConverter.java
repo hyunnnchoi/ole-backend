68047ed7dfd6b085010d2b86258cbc799bcdc95f
imhyojin <gywls5184@naver.com>
2024-01-20 05:12:00 +0900

    feat:비회원 신청 내역 조회 API 구현 #6

--- src/main/java/com/example/olebackend/converter/NonMemberConverter.java
+++ src/main/java/com/example/olebackend/converter/NonMemberConverter.java
@@ -2,8 +2,13 @@ package com.example.olebackend.converter;
 
 import com.example.olebackend.domain.Lesson;
 import com.example.olebackend.domain.NonMember;
+import com.example.olebackend.domain.mapping.LectureTeacher;
+import com.example.olebackend.web.dto.LessonResponse;
 import com.example.olebackend.web.dto.NonMemberResponse;
 import com.example.olebackend.web.dto.NonMemberRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class NonMemberConverter {
     public static NonMemberResponse.ApplyResultDTO toApplyresultDTO(NonMember nonMember){
@@ -19,6 +24,24 @@ public class NonMemberConverter {
                 .name(request.getName())
                 .email(request.getPhoneNum())
                 .phoneNum(request.getPhoneNum())
+                .build();
+    }
+
+    public static NonMemberResponse.getApplicationsResultDTO toApplications(Lesson lesson){
+
+        List<LectureTeacher> teacherList = lesson.getLectureTeacherList();
+
+        List<LessonResponse.getTeacherNameDTO> lessonTeacherList = teacherList.stream()
+                .map(LessonConverter::toTeacherNameDTO).collect(Collectors.toList());
+
+        return NonMemberResponse.getApplicationsResultDTO.builder()
+                .title(lesson.getTitle())
+                .lessonStartDate(lesson.getLessonStartDate())
+                .lessonEndDate(lesson.getLessonEndDate())
+                .lessonStartTime(lesson.getLessonStartTime())
+                .lessonEndTime(lesson.getLessonEndTime())
+                .lessonTeacherList(lessonTeacherList)
+                .place(lesson.getPlace())
                 .build();
     }
 }
--- src/main/java/com/example/olebackend/web/controller/NonMemberController.java
+++ src/main/java/com/example/olebackend/web/controller/NonMemberController.java
@@ -1,14 +1,19 @@
 package com.example.olebackend.web.controller;
 
 import com.example.olebackend.apiPayLoad.ApiResponse;
+import com.example.olebackend.converter.LessonConverter;
 import com.example.olebackend.converter.NonMemberConverter;
+import com.example.olebackend.domain.Lesson;
 import com.example.olebackend.domain.NonMember;
 import com.example.olebackend.service.NonMemberService;
+import com.example.olebackend.web.dto.LessonResponse;
 import com.example.olebackend.web.dto.NonMemberRequest;
 import com.example.olebackend.web.dto.NonMemberResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -26,6 +31,13 @@ public class NonMemberController {
 
         NonMemberResponse.ApplyResultDTO resultDto = NonMemberConverter.toApplyresultDTO(nonMember);
         return ApiResponse.onSuccess(resultDto);
+    }
+
+    @GetMapping("/guest/lessons")
+    @Operation(summary = "비회원 신청 내역 조회 API")
+    public ApiResponse<NonMemberResponse.getApplicationsResultDTO> getLessonDetail(@RequestParam String phoneNum) {
+        Lesson lesson = nonMemberService.getApplications(phoneNum);
+        return ApiResponse.onSuccess(NonMemberConverter.toApplications(lesson));
     }
 
 }
