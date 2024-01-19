4ce4053b652d145d75cbd39e159dec9bcdf29685
jiinkyung <jinkyung010310@gmail.com>
2024-01-19 23:26:25 +0900

    feat: 교육 상세 조회 관련 예외 처리 #3

--- src/main/java/com/example/olebackend/service/LessonService.java
+++ src/main/java/com/example/olebackend/service/LessonService.java
@@ -1,12 +1,16 @@
 package com.example.olebackend.service;
 
+import com.example.olebackend.apiPayLoad.exception.GeneralException;
 import com.example.olebackend.domain.Lesson;
 import com.example.olebackend.repository.LessonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
+
+import static com.example.olebackend.apiPayLoad.code.status.ErrorStatus.*;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,10 @@ public class LessonService {
     private final LessonRepository lessonRepository;
 
     public Optional<Lesson> getLessonDetail(Long lessonId) {
-       return lessonRepository.findById(lessonId);
+        Optional<Lesson> lesson = lessonRepository.findById(lessonId);
+        if (!lessonRepository.existsById(lessonId)) {
+            throw new GeneralException(LESSON_NOT_FOUND);
+        }
+        return lessonRepository.findById(lessonId);
     }
 }
