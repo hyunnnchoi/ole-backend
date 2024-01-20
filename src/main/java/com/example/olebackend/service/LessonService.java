2e807781c108bfaff1a8efca5d539cd2f3ed4988
jiinkyung <jinkyung010310@gmail.com>
2024-01-20 23:33:25 +0900

    feat: 카테고리별 교육 조회 API 구현 #12

--- src/main/java/com/example/olebackend/service/LessonService.java
+++ src/main/java/com/example/olebackend/service/LessonService.java
@@ -4,6 +4,8 @@ import com.example.olebackend.apiPayLoad.exception.GeneralException;
 import com.example.olebackend.domain.Lesson;
 import com.example.olebackend.repository.LessonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,5 +27,10 @@ public class LessonService {
             throw new GeneralException(LESSON_NOT_FOUND);
         }
         return lesson;
+    }
+
+    public Page<Lesson> getLessonListByCategory(Long categoryId, Integer page) {
+        Page<Lesson> lessonList = lessonRepository.findLessonsByCategoryId(categoryId, PageRequest.of(page, 10));
+        return lessonList;
     }
 }
