1ee8c480debe432aa05fc183dab61cee0648cec8
imhyojin <gywls5184@naver.com>
2024-01-20 16:10:06 +0900

    fix:403에러 해결 #2

--- src/main/java/com/example/olebackend/web/controller/NonMemberController.java
+++ src/main/java/com/example/olebackend/web/controller/NonMemberController.java
@@ -1,19 +1,15 @@
 package com.example.olebackend.web.controller;
 
 import com.example.olebackend.apiPayLoad.ApiResponse;
-import com.example.olebackend.converter.LessonConverter;
 import com.example.olebackend.converter.NonMemberConverter;
 import com.example.olebackend.domain.Lesson;
 import com.example.olebackend.domain.NonMember;
 import com.example.olebackend.service.NonMemberService;
-import com.example.olebackend.web.dto.LessonResponse;
 import com.example.olebackend.web.dto.NonMemberRequest;
 import com.example.olebackend.web.dto.NonMemberResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 
 @RestController
@@ -22,15 +18,15 @@ public class NonMemberController {
     private final NonMemberService nonMemberService;
 
     @PostMapping("/lesson/{lessonId}/guest")
-    @Operation(summary = "교육 신청하기(비로그인) API")
-    public ApiResponse<NonMemberResponse.ApplyResultDTO> apply(
-            @RequestBody NonMemberRequest.ApplyDTO request,
-            @PathVariable Long lessonId){
+        @Operation(summary = "교육 신청하기(비로그인) API")
+        public ApiResponse<NonMemberResponse.ApplyResultDTO> apply(
+                @RequestBody NonMemberRequest.ApplyDTO request,
+                @PathVariable Long lessonId){
 
-        NonMember nonMember = nonMemberService.applyLesson(request, lessonId);
+            NonMember nonMember = nonMemberService.applyLesson(request, lessonId);
 
-        NonMemberResponse.ApplyResultDTO resultDto = NonMemberConverter.toApplyresultDTO(nonMember);
-        return ApiResponse.onSuccess(resultDto);
+            NonMemberResponse.ApplyResultDTO resultDto = NonMemberConverter.toApplyresultDTO(nonMember);
+            return ApiResponse.onSuccess(resultDto);
     }
 
     @GetMapping("/guest/lessons")
