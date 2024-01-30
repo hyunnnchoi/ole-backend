9a301e0cfb69194f9acefc5ec4e608212d626c74
imhyojin <gywls5184@naver.com>
2024-01-30 12:28:11 +0900

    fx: healthCheck API 필요없는 코드 삭제 #71

--- src/main/java/com/example/olebackend/web/controller/RootController.java
+++ src/main/java/com/example/olebackend/web/controller/RootController.java
@@ -1,6 +1,5 @@
 package com.example.olebackend.web.controller;
 
-import com.example.olebackend.apiPayLoad.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
